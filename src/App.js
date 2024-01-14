e4f004bb0e09a1aeb2cd47138434dd9f04380d14
RohitMalviya <rohitbmalviya@gmail.com>
2024-01-15 01:33:07 +0530

    Langing Page Done 80%

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box } from "@mui/material";
+// import Navbar from "./Components/LangingPage/Navbar.js";
+// import Footer from "./Components/LangingPage/Footer.js";
+// import Banner from "./Components/LangingPage/Feed/Banner.js";
+// import Section1 from "./Components/LangingPage/Feed/Section1.js";
+// import Section2 from "./Components/LangingPage/Feed/Section2.js";
+// import Section3 from "./Components/LangingPage/Feed/Section3.js";
+// import Section4 from "./Components/LangingPage/Feed/Section4.js";
+// import Section5 from "./Components/LangingPage/Feed/Section5.js";
+// import Section6 from "./Components/LangingPage/Feed/Section6.js";
+// import Section7 from "./Components/LangingPage/Feed/Section7.js";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import Selectcity from "./Components/SelectCity/Selectcity";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Box>
+        <Navbar />
+        <Box>
+          <Section1 />
+          <Section2 />
+          <Section3 />
+          <Section4 />
+          <Banner />
+          <Section5 />
+          <Section6 />
+          <Section7 />
+        </Box>
+        <Footer />
+      </Box> */}
+      <Selectcity />
+    </>
   );
 }
 
---
+++ src/Components/LangingPage/Feed/Section7.js
@@ -0,0 +1,126 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardActionArea,
+  CardMedia,
+  Divider,
+  CardActions,
+  Button,
+  Box,
+} from "@mui/material";
+import Carousel from "react-material-ui-carousel";
+
+import Image1 from "../../Images/Section1.1.png";
+import PlaceIcon from "@mui/icons-material/Place";
+import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
+
+const Section7 = () => {
+  const Section7Img = [
+    {
+      name: "abc",
+      images: Image1,
+      desc: "The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to Naviglio where you can enjoy the main night life in Barcelona.",
+      Heading: "Southern Star Deccan Run 2023",
+      price: 889,
+      Location: "Campus,Pune",
+    },
+    {
+      name: "qwe",
+      images: Image1,
+      desc: "The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to Naviglio where you can enjoy the main night life in Barcelona.",
+      Heading: "Southern Star Deccan Run 2023",
+      price: 889,
+      Location: "Campus,Pune",
+    },
+    {
+      name: "asd",
+      images: Image1,
+      desc: "The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to Naviglio where you can enjoy the main night life in Barcelona.",
+      Heading: "Southern Star Deccan Run 2023",
+      price: 889,
+      Location: "Campus,Pune",
+    },
+    {
+      name: "zxc",
+      images: Image1,
+      desc: "The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to Naviglio where you can enjoy the main night life in Barcelona.",
+      Heading: "Southern Star Deccan Run 2023",
+      price: 889,
+      Location: "Campus,Pune",
+    },
+  ];
+
+  return (
+    <Box
+      sx={{
+        justifyContent: "center",
+        alignItems: "center",
+        margin: 2,
+        padding: 2,
+      }}
+    >
+      <Box sx={{ margin: 2 }}>
+        <Typography variant="h4">Tours Events</Typography>
+        <Typography variant="body1">Lorem Ipsum sit dolor amet.</Typography>
+      </Box>
+      <Box>
+        <Carousel
+          autoPlay={false}
+          animation="slide"
+          indicators={true}
+          navButtonsAlwaysVisible={true}
+        >
+          {Section7Img.map((photo, index) => (
+            <Card
+              key={index}
+              sx={{
+                maxWidth: "350px",
+                maxHeight: "700px",
+                margin: 2,
+                marginLeft: 3,
+                display: "flex",
+                padding: "auto",
+                borderRadius: 4,
+              }}
+            >
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  src={photo.images}
+                  alt={photo.name}
+                  sx={{ width: "330px" }}
+                />
+                <CardContent>
+                  <Typography variant="h6">{photo.Heading}</Typography>
+                </CardContent>
+                <Divider />
+                <CardActions
+                  sx={{
+                    justifyContent: "space-between",
+                    m: "auto",
+                    alignItems: "center",
+                  }}
+                >
+                  <Button size="small">
+                    <Typography variant="h5">
+                      <CurrencyRupeeIcon />
+                      {photo.price}
+                    </Typography>
+                  </Button>
+                  <Button size="small">
+                    <PlaceIcon fontSize="small" />
+                    <Typography variant="body2">{photo.Location}</Typography>
+                  </Button>
+                </CardActions>
+              </CardActionArea>
+            </Card>
+          ))}
+        </Carousel>
+      </Box>
+    </Box>
+  );
+};
+
+export default Section7;
